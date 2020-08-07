b94071e0515c98b4e93fd0809213c26c9ac0ab1e
LeeSiYoung-16 <tldud8296@gmail.com>
2020-08-08 02:20:26 +0900

    보스HP감소테스트

--- FrameDirectX12/Client/Code/BossHPBar.cpp
+++ FrameDirectX12/Client/Code/BossHPBar.cpp
@@ -72,6 +72,13 @@ _int CBossHPBar::LateUpdate_GameObject(const _float& fTimeDelta)
 
 	FAILED_CHECK_RETURN(m_pRenderer->Add_Renderer(CRenderer::RENDER_UI, this), -1);
 
+	// Å×½ºÆ® 
+	if (CDirectInput::Get_Instance()->KEY_PRESSING(DIK_N))
+	{
+	//	if (m_fPreHp > m_fCurHp)
+			m_fPreHp -= 15.f * fTimeDelta;
+	}
+
 	m_matHPWorld._41 = m_fPreHp * (0.008f);
 
 	return NO_EVENT;
